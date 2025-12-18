593eb5da64cb6b726101042a3bb25f075a1f3004
Marti-S <mstaerfeldt@gmail.com>
2025-12-18 10:25:08 +0100

    Exclude <string>-dependent headers from Arch module on Windows

--- Sources/Arch/include/Arch/Arch.h
+++ Sources/Arch/include/Arch/Arch.h
@@ -14,34 +14,42 @@
 #include <Arch/align.h>
 #include <Arch/attributes.h>
 #include <Arch/buildMode.h>
-#include <Arch/daemon.h>
 #include <Arch/debugger.h>
 #include <Arch/defines.h>
-#include <Arch/demangle.h>
-#include <Arch/env.h>
-#include <Arch/error.h>
 #include <Arch/export.h>
-#include <Arch/fileSystem.h>
-#include <Arch/function.h>
 #include <Arch/functionLite.h>
 #include <Arch/hash.h>
 #include <Arch/hints.h>
-#include <Arch/library.h>
-#include <Arch/mallocHook.h>
 #include <Arch/pragmas.h>
-#include <Arch/stackTrace.h>
-#include <Arch/symbols.h>
-#include <Arch/systemInfo.h>
 #include <Arch/testArchAbi.h>
 #include <Arch/testArchUtil.h>
 #include <Arch/threads.h>
 #include <Arch/timing.h>
+#include <Arch/inttypes.h>
+#include <Arch/math.h>
+
+// Note: The following headers are excluded from the Arch module on Windows
+// because they include <string> which triggers ICU module loading via the MSVC STL's
+// <cuchar> -> <uchar.h> chain. ICU's module map has header search path issues that
+// prevent it from building correctly when triggered indirectly.
+// These headers are still available for direct C++ inclusion in .cpp files.
+#if !defined(_WIN32)
+#include <Arch/daemon.h>
+#include <Arch/demangle.h>
+#include <Arch/env.h>
+#include <Arch/error.h>
+#include <Arch/fileSystem.h>
+#include <Arch/function.h>
+#include <Arch/library.h>
+#include <Arch/mallocHook.h>
+#include <Arch/stackTrace.h>
+#include <Arch/symbols.h>
+#include <Arch/systemInfo.h>
 #include <Arch/virtualMemory.h>
 #include <Arch/vsnprintf.h>
 #include <Arch/errno.h>
-#include <Arch/inttypes.h>
-#include <Arch/math.h>
 #include <Arch/regex.h>
+#endif
 #else // !defined(__cplusplus)
 #include <Arch/defines.h>
 #include <Arch/export.h>
